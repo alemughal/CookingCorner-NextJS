a4041ef259944909e8b7ec41b1029c4c1490dd62
alemughal <alimug38@gmail.com>
2023-06-21 12:20:10 +0500

    CONTENTFUL_ACCESS_TOKEN

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/">
+          <h1>
+            <span>Cooking</span>
+            <span>Corner</span>
+          </h1>
+          <h2>Spread The Joy</h2>
+        </Link>
+      </header>
+
+      <div className="page-content">{children}</div>
+
+      <footer>
+        <p>Copyright 2023 Cooking Corner :)</p>
+      </footer>
+    </div>
+  );
+}
