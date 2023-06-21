416c5c330a88d48692cae7a2aeea41dccf90b795
alemughal <alimug38@gmail.com>
2023-06-21 12:38:56 +0500

    Creating Recipe Card

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import { createClient } from 'contentful'
+import { createClient } from "contentful";
+import RecipeCard from "@/components/RecipeCard";
 
 export async function getStaticProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.CONTENT_SPACE_ACCESS_TOKEN,
-  })
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
 
-  const res = await client.getEntries({ content_type: 'recipe' })
+  const res = await client.getEntries({ content_type: "recipe" });
 
   return {
     props: {
       recipes: res.items,
     },
-  }
-
-  }
-
+  };
+}
 
 export default function Recipes({ recipes }) {
-  console.log(recipes)
+  console.log(recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe}/>
+      ))}
     </div>
-  )
+  );
 }
